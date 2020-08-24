bd58e2e00e49a22e6185191213a7448ed45a47b5
Kate Tester <katemorris@gmail.com>
2020-08-24 09:40:55 -0600

    Adds Get all from category method and test

--- test/potluck_test.rb
+++ test/potluck_test.rb
@@ -5,24 +5,44 @@ require './lib/potluck'
 
 class PotluckTest < MiniTest::Test
 
-  def setup
-    @potluck = Potluck.new("7-13-18")
-  end
-
   def test_it_exists
-    assert_equal "7-13-18", @potluck.date
-    assert_equal [], @potluck.dishes
+    potluck = Potluck.new("7-13-18")
+
+    assert_equal "7-13-18", potluck.date
+    assert_equal [], potluck.dishes
   end
 
   def test_add_dishes
+    potluck = Potluck.new("7-13-18")
+
     couscous_salad = Dish.new("Couscous Salad", :appetizer)
     cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
 
-    @potluck.add_dish(couscous_salad)
-    @potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(cocktail_meatballs)
 
-    assert_equal [couscous_salad, cocktail_meatballs], @potluck.dishes
-    assert_equal 2, @potluck.dishes.length
+    assert_equal [couscous_salad, cocktail_meatballs], potluck.dishes
+    assert_equal 2, potluck.dishes.length
+  end
+
+  def test_get_all_dishes_category
+    potluck = Potluck.new("7-13-18")
+
+    couscous_salad = Dish.new("Couscous Salad", :appetizer)
+    summer_pizza = Dish.new("Summer Pizza", :appetizer)
+    roast_pork = Dish.new("Roast Pork", :entre)
+    cocktail_meatballs = Dish.new("Cocktail Meatballs", :entre)
+    candy_salad = Dish.new("Candy Salad", :dessert)
+
+    potluck.add_dish(couscous_salad)
+    potluck.add_dish(summer_pizza)
+    potluck.add_dish(roast_pork)
+    potluck.add_dish(cocktail_meatballs)
+    potluck.add_dish(candy_salad)
+
+    assert_equal [couscous_salad, summer_pizza], potluck.get_all_from_category(:appetizer)
+    assert_equal couscous_salad, potluck.get_all_from_category(:appetizer).first
+    assert_equal "Couscous Salad", potluck.get_all_from_category(:appetizer).first.name
   end
 
 end
